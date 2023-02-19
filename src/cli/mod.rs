951c489eb333aaeb656c8b95303136f8ecf3d85c
Seto Elkahfi <hej@setoelkahfi.se>
2023-02-19 16:33:47 +0100

    Add log level

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -3,6 +3,10 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[clap(author, version, about)]
 pub struct Cli {
+    /// Log level: trace, debug, info, warn, error, off
+    #[clap(short, long, global = true)]
+    pub log_level: Option<String>,
+
     #[clap(subcommand)]
     pub command: Commands,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
-use anyhow::Result;
+use std::{fs::OpenOptions, path::PathBuf, str::FromStr};
+
+use anyhow::{anyhow, Result};
 use clap::Parser;
 use smbpndk_cli::{
     cli::{Cli, Commands},
     login::{process_login, LoginArgs},
 };
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{filter::LevelFilter, prelude::*, EnvFilter};
+
+fn setup_logging(level: Option<EnvFilter>) -> Result<()> {
+    // Log in the current directory
+    let log_path = PathBuf::from("smbpndk-cli.log");
+
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(log_path)
+        .unwrap();
+
+    let env_filter = if let Some(filter) = level {
+        filter
+    } else {
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("trace"))
+    };
+
+    let formatting_layer = BunyanFormattingLayer::new("smb".into(), file);
+    let level_filter = LevelFilter::from_str(&env_filter.to_string())?;
+
+    let subscriber = tracing_subscriber::registry()
+        .with(formatting_layer.with_filter(level_filter))
+        .with(JsonStorageLayer);
+
+    set_global_default(subscriber).expect("Failed to set global default subscriber");
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
@@ -20,6 +53,22 @@ async fn main() {
 
 async fn run() -> Result<()> {
     let cli = Cli::parse();
+
+    let log_level_error: Result<()> = Err(anyhow!(
+        "Invalid log level: {:?}.\n Valid levels are: trace, debug, info, warn, and error.",
+        cli.log_level
+    ));
+
+    if let Some(user_filter) = cli.log_level {
+        let filter = match EnvFilter::from_str(&user_filter) {
+            Ok(filter) => filter,
+            Err(_) => return log_level_error,
+        };
+        setup_logging(Some(filter))?;
+    } else {
+        setup_logging(None)?;
+    }
+
     match cli.command {
         Commands::Login { username, password } => {
             println!("Login: {}, {}", username, password);
