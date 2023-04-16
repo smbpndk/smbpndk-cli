c59d37c6d7c173576c95a56f2b13c63620b697a2
Seto Elkahfi <setoelkahfi@gmail.com>
2023-04-16 15:17:01 +0200

    Format and clippy

--- smbpndk-utils/src/lib.rs
+++ smbpndk-utils/src/lib.rs
@@ -1,4 +1,9 @@
+use std::{fs::OpenOptions, io::Write};
+
+use anyhow::{anyhow, Result};
+use log::debug;
 use regex::Regex;
+use smbpndk_model::Config;
 
 pub fn email_validation(input: &str) -> Result<(), &'static str> {
     let email_regex = Regex::new(
@@ -9,6 +14,23 @@ pub fn email_validation(input: &str) -> Result<(), &'static str> {
     if email_regex.is_match(input) {
         Ok(())
     } else {
-        Err("Username must be an email address")
+        Err("Username must be an email address.")
+    }
+}
+
+pub fn write_config(config: Config) -> Result<Config> {
+    match home::home_dir() {
+        Some(path) => {
+            debug!("{}", path.to_str().unwrap());
+            let mut file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .open([path.to_str().unwrap(), "/.smb/config.json"].join(""))?;
+            let json = serde_json::to_string(&config)?;
+            file.write_all(json.as_bytes())?;
+
+            Ok(config)
+        }
+        None => Err(anyhow!("Error getting config.")),
     }
 }
