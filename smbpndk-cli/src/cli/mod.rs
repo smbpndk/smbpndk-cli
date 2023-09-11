914bd27fdc1d27739476af7a805ad8cd9c8dc470
Seto Elkahfi <hej@setoelkahfi.se>
2023-09-11 14:24:21 +0200

    Cleanup

--- smbpndk-cli/src/cli/mod.rs
+++ smbpndk-cli/src/cli/mod.rs
@@ -26,11 +26,13 @@ pub enum Commands {
         command: project::cli::Commands,
     },
 
+    /*
     #[clap(about = "Manage your Oten authentication app. Add, delete, edit. Need authentication.")]
     Oten {
         #[clap(subcommand)]
         command: app_oten::cli::Commands,
     },
+     */
     // Function
     /*
     FunApp {
